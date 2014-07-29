91cf228e7d5c0d3fafebc29baf3dd6f6e652756c
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-07-29 12:05:01 -0300

    fixes palysound

--- .git_sounds/playsound.py
+++ .git_sounds/playsound.py
@@ -6,11 +6,11 @@ from random import choice
 
 def main():
     files = []
-    for root, dirs, file_names in os.walk('~/.git_sounds'):
+    for root, dirs, file_names in os.walk(os.environ['HOME']+'/.git_sounds'):
         for file_name in file_names:
             if file_name[-4:] == '.wav':
                 files.append(os.path.join(root, file_name))
-    print choice(files)
+    print choice(files) if files else ''
 
 
 if __name__ == '__main__':
